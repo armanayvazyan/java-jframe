36a5bd5b907a792479ee59a1f56d971ec3fddb63
Arman Ayvazyan <arman.zet@bk.ru>
2019-12-02 22:37:24 +0400

    added new methods

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Calculator extends JFrame {
+
+    public void setPanel(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+    }
+
+
+
+}
---
+++ src/com/company/JCalc.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JCalc {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JCalc");
+        frame.setSize(300,400);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
